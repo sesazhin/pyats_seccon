dd7da53e7d669f7a48b18814be30b80ec0a60bc3
sesazhin <sesazhin@cisco.com>
2020-11-02 01:27:22 +0300

    added email to job file

--- job_test_anyconnect.py
+++ job_test_anyconnect.py
@@ -2,7 +2,10 @@ import os
 from ats.easypy import run
 
 
-def main():
+def main(runtime):
+    mail_list = ['sesazhin@cisco.com']
+    runtime.mailbot.mailto = mail_list
+    
     # Find the location of the script in relation to the job file
     anyconnect_tests = os.path.join('anyconnect_test.py')
 
