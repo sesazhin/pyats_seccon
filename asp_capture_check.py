f0f11c6b7c3c3bcb037b7cd92ebd466ff528bb29
sesazhin <sesazhin@cisco.com>
2020-11-01 23:44:31 +0300

    removed odd code from asp_capture

--- asp_capture_check.py
+++ asp_capture_check.py
@@ -36,7 +36,7 @@ class MyCommonSetup(aetest.CommonSetup):
     """
     pass
 
-
+'''
 vpn_status_command = '/opt/cisco/anyconnect/bin/vpn status'
 vpn_disconnect_command = '/opt/cisco/anyconnect/bin/vpn disconnect'
 vpn_connect_command = '/home/admin/pyats/vpn_connect.sh'
@@ -80,7 +80,7 @@ def react_output_status(output_lines: List) -> bool:
                 connection_successful = False
 
     return connection_successful
-
+'''
 
 class VerifyASPCapture(anyconnect_test.VerifyAnyconnect):
     """
@@ -91,7 +91,6 @@ class MyCommonCleanup(anyconnect_test.MyCommonCleanup):
     """
     CommonCleanup class to disconnect Anyconnect after the tests
     """
-
 
 
 if __name__ == '__main__':
