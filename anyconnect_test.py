32f152cd8019a1cb47dc1cbaf5ec58bc0c940487
sesazhin <sesazhin@cisco.com>
2020-11-01 14:31:58 +0300

    final test

--- anyconnect_test.py
+++ anyconnect_test.py
@@ -110,7 +110,7 @@ class VerifyAnyconnect(aetest.Testcase):
     def anyconnect_stable_test(self):
 
         log.info(banner('Going standby for 45 seconds to check Anyconnect state afterwards'))
-        time.sleep(2)
+        time.sleep(45)
 
         output_lines = ac_run_command(vpn_status_command)
         connection_status = react_output_status(output_lines)
