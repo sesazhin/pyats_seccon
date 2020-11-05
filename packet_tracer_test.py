b525cef05d3b742b6562b8d2def82b1861232bac
sesazhin <sesazhin@cisco.com>
2020-11-05 18:44:06 +0300

    final version of packet_tracer_test.py

--- packet_tracer_test.py
+++ packet_tracer_test.py
@@ -47,7 +47,7 @@ class MyCommonSetup(aetest.CommonSetup):
 
         pass
 
-        
+
         genie_testbed = Genie.init(testbed)
         self.parent.parameters['testbed'] = genie_testbed
         device_list = []
@@ -62,7 +62,7 @@ class MyCommonSetup(aetest.CommonSetup):
             device_list.append(device)
         # Pass list of devices to testcases
         self.parent.parameters.update(dev=device_list)
-        
+
 
 '''
 parameters = {
